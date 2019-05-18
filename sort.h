8f315c3ab12676bcc0cecba3971c441e3a6aea53
Agamago <36169085+Agamago@users.noreply.github.com>
2019-05-17 22:18:46 -0700

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class sort
+{
+public:
+	sort(string txt);
+	~sort();
+
+	void insertSort(double arr[]);
+	void selectionSort(double arr[], int n);
+	void bubbleSort(double arr[]);
+  void quickSort(double arr[], int low, int high);
+  int partition(double arr[], int low, int high);
+
+	void readToArray(string txt);
+	double * arr;
+	int size;
+};
